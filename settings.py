a1da257f9e09c29e3af0a47f47eecad0e2d651b5
k-aziz <kaziz@bink.com>
2018-05-29 16:57:23 +0100

    fixed settings to load .env variables correctly

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 import os
 
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
-load_dotenv()
+load_dotenv(find_dotenv(usecwd=True))
 
 DEV_HOST = os.getenv('DEV_HOST', '0.0.0.0')
 DEV_PORT = os.getenv('DEV_PORT', '5000')
