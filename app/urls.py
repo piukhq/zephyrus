0a4c686292409185ecbbcd1709db5119945411ba
k-aziz <kaziz@bink.com>
2018-06-14 16:41:39 +0100

    flake 8

--- app/urls.py
+++ app/urls.py
@@ -9,4 +9,3 @@ api.add_resource(Auth, '/authorize', endpoint='api.authorize')
 
 api.add_resource(Amex, '/amex', endpoint='api.amex')
 api.add_resource(MasterCard, '/mastercard', endpoint='api.mastercard')
-
