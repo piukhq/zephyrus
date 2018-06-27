ef635247e20d3e3ccd8fb54608fc16d62de72f2d
k-aziz <kaziz@bink.com>
2018-06-27 13:40:22 +0100

    Use conversion mapping for amex request data to hermes field data.

--- app/utils.py
+++ app/utils.py
@@ -5,20 +5,6 @@ from app import CustomException
 from app.errors import CONNECTION_ERROR
 
 
-def format_data(data):
-    formatted_data = {
-        'payment_card_token': data['cm_alias'],
-        'time': data['transaction_time'],
-        'amount': int(float(data['transaction_amount']) * 100),   # conversion to pence
-        'mid': data['merchant_number'],
-        'third_party_id': data['offer_id'],
-        'auth_code': data['transaction_id'],
-        'currency_code': 'GBP'
-    }
-
-    return formatted_data
-
-
 def save_transaction(transaction):
     headers = {
         'Authorization': f'token {settings.SERVICE_API_KEY}'
--- settings.py
+++ settings.py
@@ -12,6 +12,6 @@ SERVICE_API_KEY = os.getenv('SERVICE_API_KEY', 'F616CE5C88744DD52DB628FAD8B3D')
 
 REDIS_URL = os.getenv('ZEPHYRUS_REDIS_URL', 'redis://localhost:6379/8')
 
-CLIENT_INFO_STORAGE_TIMEOUT = 1/60    # Minimum number of minutes before redis cache can be updated by hermes
+CLIENT_INFO_STORAGE_TIMEOUT = 60    # Minimum number of minutes before redis cache can be updated by hermes
 
 SIGNATURE_SECRET = os.getenv('SIGNATURE_SECRET', 'EL411REgqoW8q-Z8ncZ0m88bkxL9CQfRHT4sSHOJ0RE')
