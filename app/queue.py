9e6396118f9117fefc1031ad94fb798f25971a12
Chris Latham <backwardspy@gmail.com>
2019-09-04 09:33:15 +0100

    implement connection retries and logging

--- app/queue.py
+++ app/queue.py
@@ -1,9 +1,21 @@
+import logging
+
 from kombu import Connection
 
 import settings
 
 
+log = logging.getLogger(__name__)
+
+
+def _on_error(exc, interval):
+    log.warning(f"Failed to connect to RabbitMQ: {exc}. Will retry after {interval:.1f}s...")
+
+
 def add(message: dict, *, provider: str) -> None:
-    with Connection(settings.AMQP_DSN) as conn:
+    with Connection(settings.AMQP_DSN, connect_timeout=3) as conn:
+        conn.ensure_connection(
+            errback=_on_error, max_retries=3, interval_start=0.2, interval_step=0.4, interval_max=1, timeout=3
+        )
         q = conn.SimpleQueue(settings.AMQP_QUEUE)
         q.put(message, headers={"X-Provider": provider})
