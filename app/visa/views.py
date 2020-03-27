f52a0e7a9443d0e70aca3513428be84b20d8b077
Andrzej Mazurek <amazurek@bink.com>
2020-03-27 18:31:07 +0100

    Added visa views.py with changes to two provider names visa-auth and visa-settlement

--- app/visa/views.py
+++ app/visa/views.py
@@ -4,5 +4,10 @@ from app import queue
 
 class VisaView:
     def on_post(self, req: falcon.Request, resp: falcon.Response):
-        queue.add(req.media, provider="visa")
+        user_fields_collection = req.media.get("UserDefinedFieldsCollection")
+        if user_fields_collection and user_fields_collection[0].get("Value") == "Auth":
+            provider = "visa-auth"
+        else:
+            provider = "visa-settlement"
+        queue.add(req.media, provider=provider)
         resp.media = {"error_msg": "", "status_code": "0"}
