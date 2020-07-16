1320de767bc893278a5942dbcbed907dcb4ae2a3
Chris Latham <cl@bink.com>
2020-07-16 16:16:57 +0000

    make visa auth check case insensitive

--- app/visa/views.py
+++ app/visa/views.py
@@ -5,7 +5,7 @@ from app import queue
 class VisaView:
     def on_post(self, req: falcon.Request, resp: falcon.Response):
         user_fields_collection = req.media.get("UserDefinedFieldsCollection")
-        if user_fields_collection and user_fields_collection[0].get("Value") == "Auth":
+        if user_fields_collection and user_fields_collection[0].get("Value").lower() == "auth":
             provider = "visa-auth"
         else:
             provider = "visa-settlement"
