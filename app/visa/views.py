83d8c7308cd7b0ddbe44d6ad12b8fddef06e54ee
Chris Latham <backwardspy@gmail.com>
2021-01-07 11:44:37 +0000

    handle visa tx types that aren't auth or settle

--- app/visa/views.py
+++ app/visa/views.py
@@ -1,16 +1,33 @@
+import logging
+
 import falcon
+
 from app import queue
 from app.prometheus import counter
+
+log = logging.getLogger(__name__)
 
 
 class VisaView:
     def on_post(self, req: falcon.Request, resp: falcon.Response):
         provider = "visa"
-        user_fields_collection = req.media.get("UserDefinedFieldsCollection")
-        if user_fields_collection and user_fields_collection[0].get("Value").lower() == "auth":
+
+        # we always succeed the request, even if something fails later.
+        resp.media = {"error_msg": "", "status_code": "0"}
+
+        try:
+            vop_tx_type = req.media["UserDefinedFieldsCollection"][0]["Value"].lower()
+        except Exception as ex:
+            log.warning(f"Couldn't find UserDefinedFieldsCollection.0.Value for Visa transaction: {repr(ex)}")
+            return
+
+        if vop_tx_type == "auth":
             tx_type = "auth"
-        else:
+        elif vop_tx_type == "settle":
             tx_type = "settlement"
+        else:
+            log.warning(f"Received an unsupported {vop_tx_type} Visa transaction: {req.media}")
+            return
+
         counter.labels(payment_card=provider, type=tx_type).inc()
         queue.add(req.media, provider=provider, queue_name=f"{provider}-{tx_type}")
-        resp.media = {"error_msg": "", "status_code": "0"}
