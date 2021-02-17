151a62912b02a2184bba335047dc417e861d8ea9
mlatham <ml@bink.com>
2021-02-17 15:09:32 +0000

    mer-843 Amex settlement endpoint - fix type checking

--- app/tests/unit/test_jwt_auth.py
+++ app/tests/unit/test_jwt_auth.py
@@ -3,12 +3,13 @@ import jose.jwt
 import settings
 from unittest import mock
 from falcon.testing import TestCase
-from freezegun import freeze_time, configure
+import freezegun
+from freezegun import freeze_time
 from app import create_app
 from app.security import generate_jwt, load_secrets, validate_credentials
 
 # Freezegun ignore modules.
-configure(extend_ignore_list=["eight"])
+freezegun.configure(extend_ignore_list=["eight"])  # type: ignore
 
 
 @mock.patch("app.queue.add")
