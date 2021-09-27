dcaceb295a3e04364fcafbf86bba226f08f9e051
Terry Cain <tcain@bink.com>
2021-09-27 09:08:23 +0000

    Added log filtering

---
+++ app/tests/unit/test_gunicorn_logging.py
@@ -0,0 +1,20 @@
+import logging
+
+import pytest
+
+import app.gunicorn
+
+
+@pytest.mark.parametrize(
+    "logline,should_log",
+    [
+        ("GET /livez HTTP/1.1", False),
+        ("GET /metrics HTTP/1.1", False),
+        ("POST /auth_transactions/visa HTTP/1.1", True),
+    ],
+)
+def test_log_filtering(logline, should_log):
+    log_filter = app.gunicorn.HealthcheckFilter("filter")
+    log_record = logging.LogRecord("name", logging.INFO, "/", 1, logline, {}, None)
+
+    assert log_filter.filter(log_record) is should_log
