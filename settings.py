a8683e441b86aa0340544489d6f6b3e4bf78cd4f
Bilal Leadbitter <32436173+Mylus1@users.noreply.github.com>
2024-02-27 11:57:52 +0000

    feat: migrate from pipenv to poetry
    Co-authored-by: backwardspy <backwardspy@pigeon.life>

--- settings.py
+++ settings.py
@@ -1,20 +1,17 @@
 import logging
+import os
 
-from environment import env_var, read_env
-
-read_env()
-
-LOG_LEVEL = getattr(logging, env_var("LOG_LEVEL", "DEBUG").upper())
+LOG_LEVEL = getattr(logging, os.getenv("LOG_LEVEL", "DEBUG").upper())
 
 logging.basicConfig(format="%(asctime)s | %(name)24s | %(levelname)8s | %(message)s", level=LOG_LEVEL)
 
-DEV_HOST = env_var("DEV_HOST", "0.0.0.0")
-DEV_PORT = int(env_var("DEV_PORT", "9000"))
+DEV_HOST = os.getenv("DEV_HOST", "localhost")
+DEV_PORT = int(os.getenv("DEV_PORT", "9000"))
 
-AMQP_USER = env_var("AMQP_USER", "guest")
-AMQP_PASSWORD = env_var("AMQP_PASSWORD", "guest")
-AMQP_HOST = env_var("AMQP_HOST", "localhost")
-AMQP_PORT = env_var("AMQP_PORT", "5672")
-AMQP_DSN = env_var("AMQP_DSN", f"amqp://{AMQP_USER}:{AMQP_PASSWORD}@{AMQP_HOST}:{AMQP_PORT}//")
+AMQP_USER = os.getenv("AMQP_USER", "guest")
+AMQP_PASSWORD = os.getenv("AMQP_PASSWORD", "guest")
+AMQP_HOST = os.getenv("AMQP_HOST", "localhost")
+AMQP_PORT = os.getenv("AMQP_PORT", "5672")
+AMQP_DSN = os.getenv("AMQP_DSN", f"amqp://{AMQP_USER}:{AMQP_PASSWORD}@{AMQP_HOST}:{AMQP_PORT}//")
 
-KEYVAULT_URI = env_var("KEYVAULT_URI", None)
+KEYVAULT_URI = os.getenv("KEYVAULT_URI", None)
